ec2d0585e1da806d641c4f6b73915bfa4fceff18
ccastroelo <claudiodecastro@gmail.com>
2023-03-29 14:43:25 -0300

    seguindo o ebook do rust

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::{io, cmp::Ordering};
+use rand::Rng;
+
+fn main() {
+    
+    println!("Adivinhe o número!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("O número secreto é {secret_number}");
+    loop {
+    
+        println!("Entre o seu palpite!");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("ocorreu um erro ao ler a linha");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("seu palpite foi {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Muito pequeno!"),
+            Ordering::Greater => println!("Muito grande!"),
+            Ordering::Equal => {
+                println!("Acertou!");
+                break;
+            }
+        }
+    }
+
+}
